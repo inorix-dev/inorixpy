79fb17ff1fc6e18d66d8eb8f02e0354ce583c4b1
Shunta Komatsu <shuntak217@gmail.com>
2020-06-25 23:07:04 +0900

    :sparkles: Add tqdm printer

--- inorixpy/print.py
+++ inorixpy/print.py
@@ -1,6 +1,8 @@
 import contextlib
 import os
 
+from tqdm import tqdm
+
 
 def print_separate():
     width = int(os.popen("stty size", "r").read().split()[1])
@@ -12,3 +14,8 @@ def print_separates():
     print_separate()
     yield
     print_separate()
+
+
+def tqdm_print(*values, sep=" ", end="\n", file=None, nolock=False):
+    s = sep.join([str(v) for v in values])
+    tqdm.write(s, end=end, file=file, nolock=nolock)
